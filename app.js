ec012f99de8dcdcf9d98562eff572799ee31a3dc
Kyle McDonald <kyle@kylemcdonald.net>
2016-11-12 21:18:37 -0800

    initial connection to ms api

--- app.js
+++ app.js
@@ -1,13 +1,72 @@
+var _ = require('lodash');
+var http = require('http');
 var express = require('express');
 var app = express();
 
+function getDescription(url, cb) {
+	var postData = JSON.stringify({
+	 	'url' : url
+	});
+
+	var options = {
+		hostname: 'api.projectoxford.ai',
+		port: 80,
+		path: '/vision/v1.0/describe',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Ocp-Apim-Subscription-Key': process.env.OCP_API_KEY
+		}
+	};
+
+	var req = http.request(options, (res) => {
+		if(res.statusCode == 200) {
+			res.setEncoding('utf8');
+			res.on('data', (chunk) => {
+				var data = JSON.parse(chunk);
+				console.log(data);
+				cb(data.description.captions[0].text);
+			});
+		} else {
+			cb();
+		}
+	});
+
+	req.on('error', (e) => {
+		console.log(e.message);
+	});
+
+	req.write(postData);
+	req.end();
+}
+
+var recent = [];
+
 app.use('/', express.static('public'));
 
-app.get('/', function (req, res, next) {
+app.get('/', (req, res) => {
 	res.redirect('/moogfest');
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+app.get('/add', (req, res) => {
+	res.end();	
+	var img = req.query;
+	img.timestamp = new Date().getTime();
+	getDescription(img.url, (description) => {
+		img.text = description;
+		recent.push(img);
+		console.log(img);
+	})
+});
+
+app.get('/recent.json', (req, res) => {
+	res.setHeader('Content-Type', 'application/json');
+	var sorted = _.sortBy(recent, 'timestamp');
+	var last = sorted[sorted.length - 1];
+	res.json(last);
+});
+
+var server = app.listen(process.env.PORT || 3000, () => {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log('Example app listening at http://%s:%s', host, port);
--- public/idfa/js/tts.js
+++ public/idfa/js/tts.js
@@ -1,11 +1,12 @@
 var whisperVoice;
 
-window.speechSynthesis.onvoiceschanged = function() {
+window.speechSynthesis.onvoiceschanged = () => {
 	var voices = window.speechSynthesis.getVoices();
 	whisperVoice = _.find(voices, _.matchesProperty('name', 'Whisper'));
 }
 
 function whisper(text, cb) {
+	text = "I see " + text;
 	var msg = new SpeechSynthesisUtterance(text);
 	msg.voice = whisperVoice;
 	if (cb) {
@@ -14,8 +15,14 @@ function whisper(text, cb) {
 	window.speechSynthesis.speak(msg);
 }
 
-function speak() {
-	whisper('A man in a suit and tie.', function () {
-		console.log('done');
+function load() {
+	// this will need to be changed from .. to .
+	$.getJSON('../recent.json', (data) => {
+		console.log(data);
+		$('#cur-url').attr('src', data.url);
+		$('#cur-text').text(data.text);
+		whisper(data.text, () => {
+			console.log('done speaking');
+		});
 	});
 }
